5fa4fef7ea8c20fc35b593b8b4d766aa63baa3ca
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-06-07 17:11:28 +0800

    6/7

--- abner/main2.py
+++ abner/main2.py
@@ -59,7 +59,7 @@ history = model.fit(Tra_data, Tra_label, batch_size=bz,
                     validation_data=(Val_data, Val_label))
 
 loss = np.array(history.history['loss'])
-tf.keras.models.save_model(model, "saved_model.hp5", save_format="h5")
+model.save_weights('Fm04_w.h5')
 
 #%%
 pred_tes = model.predict(Tes_data)
