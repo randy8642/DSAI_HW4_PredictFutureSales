7a8408d258eafe85a7280dd4e2852faf23f1d406
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-06-09 21:48:38 +0800

    6/9

--- Data_preprocess.py
+++ Data_preprocess.py
@@ -10,15 +10,12 @@ def train_preprocess(df: pd.DataFrame):
     df = df[(df['item_price'] < 300000) & (df['item_cnt_day'] < 1000)]
     df = df[df['item_price'] > 0].reset_index(drop=True)
     df.loc[df['item_cnt_day'] < 1, 'item_cnt_day'] = 0
-
     # 合併相同商店(名稱相同)
     same_shopId = [[0, 57], [1, 58], [10, 11]]
     for pair in same_shopId:
         df.loc[df['shop_id'] == pair[0], 'shop_id'] = pair[1]
-
     # 加上銷售額
     df['revenue'] = df['item_price'] * df['item_cnt_day']
-
     return df
 
 
@@ -27,19 +24,15 @@ def test_preprocess(df: pd.DataFrame):
     same_shopId = [[0, 57], [1, 58], [10, 11]]
     for pair in same_shopId:
         df.loc[df['shop_id'] == pair[0], 'shop_id'] = pair[1]
-
     return df
 
 
 def shop_preprocess(df: pd.DataFrame):
-
     # 切割商店名稱 => 所在地 & 類型
     df['city'] = df['shop_name'].str.split(
         ' ').map(lambda x: ' '.join(x[0:-2]))
     df['category'] = df['shop_name'].str.split(' ').map(lambda x: x[-2])
-
     df['city'] = df['city'].apply(lambda x: re.sub('[!]', '', x))
-
     # 合併過小的類別
     category = []
     for cat in df['category'].unique():
@@ -47,71 +40,55 @@ def shop_preprocess(df: pd.DataFrame):
             category.append(cat)
     df['category'] = df['category'].apply(
         lambda x: x if (x in category) else "other")
-
     # 標籤(文字) => 索引(類別編號)
     _, df['shopcategory_id'] = np.unique(
         df['category'], return_inverse=True)
     _, df['shop_city_id'] = np.unique(
         df['city'], return_inverse=True)
-
     # 新df
     df = df[["shop_id", "shopcategory_id", "shop_city_id"]]
-
     return df
 
 
 def category_preprocess(df: pd.DataFrame):
-
     # 切割類別名稱 => 主類別 & 子類別
     df['cate_type'] = df['item_category_name'].apply(
         lambda x: re.split(' - ', x)[0]).astype(str)
     df['cate_subtype'] = df['item_category_name'].apply(
         lambda x: re.split(' - ', x)[-1]).astype(str)
-
-    #
     df.loc[ (df['cate_type'] == 'Игровые')| (df['cate_type'] == 'Аксессуары'), 'cate_type' ] = 'Игры'
-
     # 合併過小的類別
     category = []
     for cat in df['cate_type'].unique():
         if len(df[df['cate_type'] == cat]) >= 5:
             category.append(cat)
-
     df['cate_type'] = df['cate_type'].apply(
         lambda x: x if (x in category) else 'etc')
-
     # 標籤(文字) => 索引(類別編號)
     _, df['cate_type_id'] = np.unique(df['cate_type'], return_inverse=True)
     _, df['cate_subtype_id'] = np.unique(
         df['cate_subtype'], return_inverse=True)
-
     # 新df
     df = df[['item_category_id', 'cate_type_id', 'cate_subtype_id']]
-
     return df
 
 
 def items_preprocess(df: pd.DataFrame):
-
     # 切割商品名稱 => 類別1 & 類別2
     def splitNameType1(x):
         results = re.findall(r'\(.*', x)
-
         if len(results) > 0:
             result = re.sub('[^A-Za-z0-9А-Яа-я]+', ' ', results[0][1:]).lower()
         else:
             result = np.nan
-
         return result
 
     def splitNameType2(x):
         results = re.findall(r'\[.*', x)
-
         if len(results) > 0:
             result = re.sub('[^A-Za-z0-9А-Яа-я]+', ' ', results[0][1:]).lower()
         else:
             result = np.nan
-
         return result
 
     def name_correction(x: str):
@@ -120,21 +97,17 @@ def items_preprocess(df: pd.DataFrame):
         x = re.sub('[^A-Za-z0-9А-Яа-я]+', ' ', x)
         x = re.sub('  ', ' ', x)
         x = x.strip()
-
         return x
 
     df['item_type_1'] = df['item_name'].apply(lambda x: splitNameType1(x))
     df['item_type_2'] = df['item_name'].apply(lambda x: splitNameType2(x))
-
     df = df.fillna('0')
-
     # 修正商品名稱
     df["item_name"] = df["item_name"].apply(lambda x: name_correction(x))
-
     # 切割類別
     df["type"] = df['item_type_2'].apply(lambda x: x[0:8] if x.split(" ")[
                                          0] == "xbox" else x.split(" ")[0])
-
+    
     # 整理
     df.loc[(df['type'] == 'x360') | (df['type'] == 'xbox360')
            | (df['type'] == 'xbox 360'), 'type'] = 'xbox 360'
@@ -143,7 +116,7 @@ def items_preprocess(df: pd.DataFrame):
     df.loc[(df['type'] == 'pc') | (df['type'] == 'pс')
            | (df['type'] == 'pc'), 'type'] = 'pc'
     df.loc[df['type'] == 'рs3', 'type'] = 'ps3'
-
+    
     # 整合較小的類別
     group_sum = df.groupby(['type']).agg({'item_id': 'count'})
     group_sum = group_sum.reset_index()
@@ -161,7 +134,6 @@ def items_preprocess(df: pd.DataFrame):
 
     # 新df
     df = df[['item_id', 'item_category_id', 'item_type_1_id', 'item_type_2_id']]
-
     return df
 
 
@@ -171,19 +143,16 @@ def save_dataframe(df: pd.DataFrame):
 
 def createTotalDataframe():
     global df_train
-
     matrix = []
     cols = ["date_block_num", "shop_id", "item_id"]
     for i in range(34):
         sales = df_train[df_train['date_block_num'] == i]
         matrix.append(np.array(list(product([i], sales['shop_id'].unique(), sales['item_id'].unique())), dtype=np.int))
-
     matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
     matrix["date_block_num"] = matrix["date_block_num"].astype(np.int8)
     matrix["shop_id"] = matrix["shop_id"].astype(np.int8)
     matrix["item_id"] = matrix["item_id"].astype(np.int16)
     matrix.sort_values(cols, inplace=True)
-
     return matrix
 
 
@@ -196,7 +165,6 @@ def splitDate(df: pd.DataFrame):
     df["month"] = df["date_block_num"] % 12
     days = pd.Series([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
     df["days"] = df["month"].map(days).astype(np.int8)
-
     return df
 
 
@@ -205,11 +173,9 @@ def addMonthCnt(df: pd.DataFrame, df_src: pd.DataFrame):
         {"item_cnt_day": ["sum"]})
     group.columns = ["item_cnt_month"]
     group.reset_index(inplace=True)
-
     cols = ["date_block_num", "shop_id", "item_id"]
     df = pd.merge(df, group, on=cols, how="left")
     df["item_cnt_month"] = df["item_cnt_month"].fillna(0).astype(np.float16)
-
     return df
 
 
@@ -217,9 +183,7 @@ def addMonthAvgCnt(df: pd.DataFrame):
     group = df.groupby(["date_block_num"]).agg({"item_cnt_month": ["mean"]})
     group.columns = ['date_avg_item_cnt']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=["date_block_num"], how="left")
-
     return df
 
 
@@ -228,9 +192,7 @@ def addMonthItemAvgCnt(df: pd.DataFrame):
         {'item_cnt_month': ['mean']})
     group.columns = ['date_item_avg_item_cnt']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=['date_block_num', 'item_id'], how='left')
-
     return df
 
 
@@ -239,9 +201,7 @@ def addMonthShopAvgCnt(df: pd.DataFrame):
         {"item_cnt_month": ["mean"]})
     group.columns = ["date_shop_avg_item_cnt"]
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=["date_block_num", "shop_id"], how="left")
-
     return df
 
 
@@ -250,10 +210,8 @@ def addMonthShopItemAvgCnt(df: pd.DataFrame):
         {"item_cnt_month": ["mean"]})
     group.columns = ["date_shop_item_avg_item_cnt"]
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=["date_block_num",
                   "shop_id", "item_id"], how="left")
-
     return df
 
 
@@ -262,10 +220,8 @@ def addMonthShopsubTypeAvgCnt(df: pd.DataFrame):
         {'item_cnt_month': ['mean']})
     group.columns = ['date_shop_subtype_avg_item_cnt']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=['date_block_num',
                   'shop_id', 'cate_subtype_id'], how='left')
-
     return df
 
 
@@ -274,9 +230,7 @@ def addMonthCityAvgCnt(df: pd.DataFrame):
                        ).agg({'item_cnt_month': ['mean']})
     group.columns = ['date_city_avg_item_cnt']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=['date_block_num', "shop_city_id"], how='left')
-
     return df
 
 
@@ -285,10 +239,8 @@ def addMonthCityItemAvgCnt(df: pd.DataFrame):
         {'item_cnt_month': ['mean']})
     group.columns = ['date_item_city_avg_item_cnt']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=['date_block_num',
                   'item_id', 'shop_city_id'], how='left')
-
     return df
 
 
@@ -296,45 +248,35 @@ def addAvgPrice(df: pd.DataFrame, df_src: pd.DataFrame):
     group = df_src.groupby(["item_id"]).agg({"item_price": ["mean"]})
     group.columns = ["item_avg_item_price"]
     group.reset_index(inplace=True)
-
     df = df.merge(group, on=["item_id"], how="left")
-
     return df
 
 
 def addMonthAvgPrice(df: pd.DataFrame, df_src: pd.DataFrame):
-    group = df_src.groupby(["date_block_num", "item_id"]
-                           ).agg({"item_price": ["mean"]})
+    group = df_src.groupby(["date_block_num", "item_id"]).agg({"item_price": ["mean"]})
     group.columns = ["date_item_avg_item_price"]
     group.reset_index(inplace=True)
-
     df = df.merge(group, on=["date_block_num", "item_id"], how="left")
-
     return df
 
 
 def priceTrend(df: pd.DataFrame):
     lags = [1, 2, 3]
     df = addLag(df, 'date_item_avg_item_price', lags)
-
     for i in lags:
         df[f'delta_price_lag_{i}'] = \
             (df[f'date_item_avg_item_price_lag_{i}'] -
              df['item_avg_item_price']) / df['item_avg_item_price']
-
     def select_trends(row):
         for i in lags:
             if row[f'delta_price_lag_{i}']:
                 return row[f'delta_price_lag_{i}']
         return 0
-
     df["delta_price_lag"] = df.apply(select_trends, axis=1)
     df["delta_price_lag"].fillna(0, inplace=True)
-
     df.drop(['date_item_avg_item_price', 'item_avg_item_price'], axis=1, inplace=True)
     df.drop([f'date_item_avg_item_price_lag_{i}' for i in [1, 2, 3]], axis=1, inplace=True)
     df.drop([f'delta_price_lag_{i}' for i in [1, 2, 3]], axis=1, inplace=True)
-
     return df
 
 
@@ -344,67 +286,51 @@ def revenue(df: pd.DataFrame, df_src: pd.DataFrame):
                            ).agg({'revenue': ['sum']})
     group.columns = ['date_shop_revenue']
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=["date_block_num", "shop_id"], how="left")
-
     #
     group = group.groupby(["shop_id"]).agg({"date_block_num": ["mean"]})
     group.columns = ["shop_avg_revenue"]
     group.reset_index(inplace=True)
-
     df = pd.merge(df, group, on=['shop_id'], how="left")
-
     #
     df['shop_avg_revenue'] = df['shop_avg_revenue'].astype(np.float32)
     df['delta_revenue'] = (df['date_shop_revenue'] -
                            df['shop_avg_revenue']) / df['shop_avg_revenue']
     df['delta_revenue'] = df['delta_revenue'].astype(np.float32)
-
     #
     df = addLag(df, 'delta_revenue', [1])
     df.drop(['date_shop_revenue', 'shop_avg_revenue',
             'delta_revenue'], axis=1, inplace=True)
-
     return df
 
 
 def addLag(df: pd.DataFrame, col: str, lags: list):
-
     assert sum([type(x) == int for x in lags]) == len(
         lags), '非所有lags為type(int)'
-
     tmp = df[["date_block_num", "shop_id", "item_id", col]]
-
     for i in lags:
         shifted = copy.deepcopy(tmp)
-        shifted.columns = ["date_block_num",
-                           "shop_id", "item_id", col + "_lag_"+str(i)]
+        shifted.columns = ["date_block_num","shop_id", "item_id", col + "_lag_"+str(i)]
         shifted['date_block_num'] = shifted['date_block_num'] + i
-        df = pd.merge(df, shifted, on=[
-                      'date_block_num', 'shop_id', 'item_id'], how='left')
-
+        df = pd.merge(df, shifted, on=['date_block_num', 'shop_id', 'item_id'], how='left')
     return df
 
 
 def createTest(df: pd.DataFrame, df_src: pd.DataFrame):
     cols = ["date_block_num", "shop_id", "item_id"]
-
     df_src['date_block_num'] = 34
     df = pd.concat([df, df_src.drop(["ID"], axis=1)],  ignore_index=True, sort=False, keys=cols)
     df.fillna(0, inplace=True)
-
     return df
 
 
 def addFirstSale(df:pd.DataFrame):
     df["item_shop_first_sale"] = df["date_block_num"] - df.groupby(["item_id","shop_id"])["date_block_num"].transform('min')
     df["item_first_sale"] = df["date_block_num"] - df.groupby(["item_id"])["date_block_num"].transform('min')
-
     return df
 
 def delnanLag(df:pd.DataFrame):
     df = df[df["date_block_num"] > 3]
-
     return df
 
 #############################################################################################
