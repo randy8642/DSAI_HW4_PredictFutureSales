b197586c89fa5aca2ecf2e2d00f4401f6ea6ae90
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-06-14 19:12:28 +0800

    6/14

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     id_list = np.arange(0, len(pred_tes), 1).astype(str)
     D = np.vstack([id_list, pred_tes]).T
     df = pd.DataFrame(D, columns=["ID", "item_cnt_month"])
-    df.to_csv('XG_RY.csv', index=False)
+    df.to_csv('submission.csv', index=False)
 
 if __name__=='__main__':
     main()
